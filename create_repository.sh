ea6e195fe572bb22063c79572100af6633ea3e71
Fábio Luciano <fabio@naoimporta.com>
2014-05-14 20:10:35 -0300

    Update create_repository.sh
    Adding variables references to code instead of confuse another one

--- create_repository.sh
+++ create_repository.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
-svnadmin create --fs-type fsfs /var/svn/${1}/${2}
-chown -R apache:apache /var/svn/${1}/${2}
-chmod -R g+w /var/svn/${1}/${2}/
-chmod g+s /var/svn/${1}/${2}/db
+
+# Script for create svn repository on centos or redhat
+
+
+# Main repository location
+repository_root_location='/var/subversion'
+
+# Don't edit
+project=${1}
+repository={2}
+repository_path=$repository_root_location/$project/$repository
+
+svnadmin create --fs-type fsfs $repository_path
+chown -R apache:apache $repository_path
+chmod -R g+w $repository_path
+chmod g+s $repository_path
 
 #Atualiza contextos para o SELinux
-chcon -R -t httpd_sys_content_t /var/svn/${1}/${2}
-chcon -R -t httpd_sys_rw_content_t /var/svn/${1}/${2}
+chcon -R -t httpd_sys_content_t $repository_path
+chcon -R -t httpd_sys_rw_content_t $repository_path
